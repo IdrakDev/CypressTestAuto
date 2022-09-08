260c65299491c347f7973b7f6ca23188accede6b
Idrak <idrakmirzoyev@gmail.com>
2022-09-08 11:05:12 +0400

    Test Api

---
+++ TestSite/cypress/e2e/ApiTest/test_api_get.cy.js
@@ -0,0 +1,9 @@
+describe ('apiTest', function(){
+    it("Api", function(){
+        cy.request('GET', 'https://swapi.dev/api/people/1/').then((response) =>{
+            expect(response).to.have.property('status', 200)
+            expect(response.body).to.not.be.null
+            expect(response.body.films).to.have.length(4)
+        })
+    })
+})
